6ee6c6f4fbf0c2f05e744ba92cae0362b11cd43d
Aram Simonian <aram.contacts@gmail.com>
2019-08-30 22:02:08 +0100

    Completed test refactoring using setUp method

--- test_phonebook.py
+++ test_phonebook.py
@@ -12,10 +12,11 @@ class PhonebookTest(unittest.TestCase):
         self.assertEqual("12345", self.phonebook.lookup("Bob"))
 
     def test_missing_entry_raises_KeyError(self):
-        self.phonebook = Phonebook()
         with self.assertRaises(KeyError):
             self.phonebook.lookup("missing")
 
     def test_empty_phonebook_is_consistent(self):
-        self.phonebook = Phonebook()
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_normal_entries_is_consistent(self):
         self.assertTrue(self.phonebook.is_consistent())
