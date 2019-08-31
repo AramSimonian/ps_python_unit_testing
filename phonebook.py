dd2a7ea05cca9c93f04282b1af65bb3cac76c9f1
Aram Simonian <aram.contacts@gmail.com>
2019-08-31 13:13:02 +0100

    Minor formatting / renaming changes for readability

--- phonebook.py
+++ phonebook.py
@@ -14,7 +14,7 @@ class Phonebook:
         nums = list(self.entries.values())
         
         def dupe_name(self, name):
-            return names.count(name) > 1
+            return (names.count(name) > 1)
 
         def dupe_num(self, num):
             return (nums.count(num) > 1)
@@ -30,10 +30,10 @@ class Phonebook:
         if len(self.entries) == 0:
             return True
         else:
-            name_count = (len(list(name for name in names if dupe_name(self, name))))
-            num_count = (len(list(num for num in nums if dupe_num(self, num))))
-            prefix_count = (len(list(num for num in nums if dupe_prefix(self, num))))
+            dup_name_count = (len(list(name for name in names if dupe_name(self, name))))
+            dup_num_count = (len(list(num for num in nums if dupe_num(self, num))))
+            dup_prefix_count = (len(list(num for num in nums if dupe_prefix(self, num))))
 
-            return (name_count + num_count + prefix_count) == 0
+            return (dup_name_count + dup_num_count + dup_prefix_count) == 0
 
     
